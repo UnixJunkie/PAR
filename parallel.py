c5afc0e3999927477ff2f7fa3795b954c8d25c4d
Francois Berenger <francois.berenger.fun@gmail.com>
2009-12-14 10:42:29 +0900

    preparing for using Pyro, client/server mode is no more available for some time

--- parallel.py
+++ parallel.py
@@ -29,6 +29,9 @@ warning: keep this script compatible with python 2.4 so that we can run it
          on old systems too
 
 TODO:
+ * use Pyro instead of my beginner code
+   - use the Master
+   - use the Worker
  * test this:
    The client could be at the same time a server for other clients in order to
    scale by using hierarchical layers of servers instead of having only one
@@ -42,7 +45,7 @@ TODO:
  * for -p|--post: return both (stdin, stdout and stderr to the user), not just
    (stdin, stdout) like actually, so that he can troubleshoot if some of his
    commands fail in error by reading their stderr
- * profile this program with the python profiler
+ * profile with the python profiler
  * add a code coverage test, python is not compiled and pychecker is too
    light at ckecking things
  * correct this harmless bug (program should exit cleanly instead of
@@ -51,26 +54,49 @@ TODO:
    exception:  exceptions.ZeroDivisionError
 """
 
-import commands, select, socket, string, sys, thread
+import commands, string, sys, thread
+import Pyro.core, Pyro.naming
 
 from Queue import Queue, Empty
 from ProgressBar import ProgressBar
+from Pyro.errors import PyroError, NamingError
+
+class Master(Pyro.core.ObjBase):
+    def __init__(self, commands_q, results_q):
+        Pyro.core.ObjBase.__init__(self)
+        self.jobs_queue     = commands_q
+        self.results_queue  = results_q
+
+    def get_work(self):
+        res = ""
+        try:
+            res = self.jobs_queue.get(True, 1)
+        except Empty:
+            pass
+        return res
+
+    def put_result(self, cmd_and_res):
+        self.results_queue.put(cmd_and_res)
+
+    def add_job(self, cmd):
+        self.jobs_queue.put(cmd)
 
 def usage():
-    print ("Usage: parallel.py [options] -i | -c ...")
+#     print ("Usage: parallel.py [options] -i | -c ...")
+    print ("Usage: parallel.py [options] -i ...")
     print ("Execute commands in parallel.")
     print ("")
     print ("  [-h | --help]               you are currently reading it")
-    print ("  -c  | --client host port    BETA/EXPERIMENTAL feature")
-    print ("                              read commands from a server")
-    print ("                              instead of a file")
-    print ("                              use -c or -i, not both")
+#     print ("  -c  | --client host port    BETA/EXPERIMENTAL feature")
+#     print ("                              read commands from a server")
+#     print ("                              instead of a file")
+#     print ("                              use -c or -i, not both")
     print ("  -i  | --input commands_file /dev/stdin for example")
     print ("  [-o | --output output_file] log to a file instead of stdout")
     print ("  [-p | --post python_module] specify a post processing module")
     print ("                              (omit the '.py' extension)")
-    print ("  [-s | --server port]        BETA/EXPERIMENTAL feature")
-    print ("                              accept remote workers")
+#     print ("  [-s | --server port]        BETA/EXPERIMENTAL feature")
+#     print ("                              accept remote workers")
     print ("  [-v | --verbose]            enables progress bar")
     print ("  [-w | --workers n]          local worker threads (default is " +
            str(get_nb_procs()) + ")")
@@ -103,127 +129,12 @@ def parse_cmd_echo(cmd_and_output):
 def get_nb_procs():
     return int(commands.getoutput("egrep -c '^processor' /proc/cpuinfo"))
 
-def local_worker_wrapper(commands_queue, results_queue, lock):
-    try:
-        while (True):
-            cmd = commands_queue.get(True, 1)
-            cmd_out = commands.getoutput(cmd)
-            results_queue.put((cmd, cmd_out))
-    except Empty:
-        lock.release()
-
-def receive_all(sock, log_prefix):
-    buff = ""
-    try:
-        receive_size = 4096
-        received = sock.recv(receive_size)
-        while len(received) > 0:
-            buff += received
-            received = sock.recv(receive_size)
-    except:
-        print log_prefix, "server closed or receive timedout"
-    return buff
-
-def remote_worker_wrapper(lock, server_name, server_port):
-    log_prefix = "network worker:"
-    while True:
-        try:
-            client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_sock.connect((server_name, server_port))
-            client_sock.settimeout(5)
-        except:
-            print log_prefix + "server is down"
-            client_sock.close()
-            break # end infinite loop, server no more here
-        cmd = receive_all(client_sock, log_prefix)
-        if cmd == "":
-            client_sock.close()
-            break # end infinite loop, server no more here
-        #print "received:" + cmd
-        cmd_out = commands.getoutput(cmd)
-        try:
-            to_send = parsable_echo((cmd, cmd_out))
-            client_sock.sendall(to_send)
-            client_sock.shutdown(socket.SHUT_WR)
-            #print "sent: " + to_send
-        except:
-            print log_prefix + "problem while sending result"
-        client_sock.close()
-    #print "worker left"
-    lock.release()
-
-def server_wrapper(commands_queue, results_queue, lock, server_port):
-    nb_sent_jobs     = 0
-    nb_received_jobs = 0
-    # start server
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_sock_fd = server_sock.fileno()
-    server_open = True
-    server_name = commands.getoutput("hostname")
-    #print "server:", server_name, "port:", str(server_port)
-    server_sock.bind((server_name, server_port))
-    server_sock.listen(1)
-    server_commands = []
-    poller = select.poll()
-    poller.register(server_sock, select.POLLIN) # new clients to accept
-    sock_fd_dict = {} # socket to fd mapping
-    while server_open:
-        # sorting the poll output would give priorities to the server tasks:
-        # accept new client, send job, read result, etc.
-        for (fd, event) in poller.poll():
-            #print "### poll"
-            if fd == server_sock_fd: # accept new client
-                if server_open:
-                    client_conn, client_addr = server_sock.accept()
-                    #print "connection from " + str(client_addr)
-                    # he will want a job
-                    poller.register(client_conn, select.POLLOUT)
-                    sock_fd_dict[client_conn.fileno()] = client_conn
-            elif event & select.POLLOUT: # send job
-                try:
-                    cmd = commands_queue.get(True, 1)
-                    server_commands.append(cmd)
-                except Empty:
-                    #print "server: empty commands queue"
-                    if len(server_commands) == 0:
-                        #print "server: empty server queue"
-                        if server_open and nb_received_jobs == nb_sent_jobs:
-                            server_sock.shutdown(socket.SHUT_RDWR)
-                            poller.unregister(server_sock)
-                            server_sock.close()
-                            server_open = False
-                if len(server_commands) > 0:
-                    cmd = server_commands.pop(0)
-                    client_conn = sock_fd_dict[fd]
-                    try:
-                        client_conn.sendall(cmd)
-                        client_conn.shutdown(socket.SHUT_WR)
-                        poller.unregister(client_conn)
-                        # he will give a result
-                        poller.register(client_conn, select.POLLIN)
-                        nb_sent_jobs += 1
-                        #print "server: sent one job"
-                        #print "nb jobs sent: " + str(nb_sent_jobs)
-                    except:
-                        print "server: problem while sending command"
-                        server_commands.insert(0, cmd) # keep for next client
-                        poller.unregister(client_conn)
-                        client_conn.close() # fire client
-            elif event & select.POLLIN: # get result
-                client_conn = sock_fd_dict[fd]
-                result = receive_all(client_conn, "server:")
-                #print "server received:" + result
-                poller.unregister(client_conn)
-                client_conn.close()
-                sock_fd_dict.pop(fd)
-                nb_received_jobs += 1
-                results_queue.put(parse_cmd_echo(result))
-                #print "server: got one job"
-                #print "nb jobs received: " + str(nb_received_jobs)
-            else:
-                print "server: unhandled event"
-                sys.exit(1)
-    #print "server: all jobs done"
+def local_worker_wrapper(master, lock):
+    work = master.get_work()
+    while work != "":
+        cmd_out = commands.getoutput(work)
+        master.put_result((work, cmd_out))
+        work = master.get_work()
     lock.release()
 
 # return index in lst of the first element from elt_lst found in lst
@@ -270,7 +181,8 @@ if __name__ == '__main__':
             commands_file_param = args[input_param + 1]
             commands_file  = open(commands_file_param, 'r')
         elif remote_server_param == -1:
-            print "-i or -c is mandatory"
+#             print "-i or -c is mandatory"
+            print "-i is mandatory"
             usage() # -h | --help falls here also
         if first_index_lst(["-v","--verbose"], args) != -1:
             show_progress = True
@@ -297,44 +209,46 @@ if __name__ == '__main__':
             usage()
         commands_queue = Queue()
         results_queue  = Queue()
+        master = Master(commands_queue, results_queue)
         nb_jobs        = 0
         locks          = []
-        if local_server_port != -1:
-            l = thread.allocate_lock()
-            l.acquire()
-            locks.append(l)
-            thread.start_new_thread(server_wrapper,
-                                    (commands_queue, results_queue, l,
-                                     local_server_port))
+#         if local_server_port != -1:
+#             l = thread.allocate_lock()
+#             l.acquire()
+#             locks.append(l)
+#             thread.start_new_thread(server_wrapper,
+#                                     (commands_queue, results_queue, l,
+#                                      local_server_port))
+        if input_param != -1:
+            # read jobs from local file
+            for cmd in commands_file:
+                master.add_job(string.strip(cmd))
+                nb_jobs += 1
         if remote_server_port != -1 and remote_server_name != "":
+            pass
             # start clients of remote server
-            for i in range(nb_threads):
-                l = thread.allocate_lock()
-                l.acquire()
-                locks.append(l)
-                thread.start_new_thread(remote_worker_wrapper,
-                                        (l,
-                                         remote_server_name,
-                                         remote_server_port))
+#             for i in range(nb_threads):
+#                 l = thread.allocate_lock()
+#                 l.acquire()
+#                 locks.append(l)
+#                 thread.start_new_thread(remote_worker_wrapper,
+#                                         (l,
+#                                          remote_server_name,
+#                                          remote_server_port))
         else: # start local workers
             for i in range(nb_threads):
                 l = thread.allocate_lock()
                 l.acquire()
                 locks.append(l)
                 thread.start_new_thread(local_worker_wrapper,
-                                        (commands_queue, results_queue, l))
+                                        (master, l))
         if input_param != -1:
-            # read jobs from local file
-            for cmd in commands_file:
-                commands_queue.put(string.strip(cmd))
-                nb_jobs += 1
             progress_bar = ProgressBar(0, nb_jobs, 60)
             # output everything
             jobs_done = 0
             if show_progress:
                 progress_bar.draw()
             while jobs_done < nb_jobs:
-                #print "not finished with commands file"
                 cmd_and_output = results_queue.get()
                 jobs_done += 1
                 if output_to_file:
@@ -352,7 +266,6 @@ if __name__ == '__main__':
                         print post_proc_fun(cmd_and_output)
             # close all files
             commands_file.close()
-            #print "finished with commands file"
             if output_to_file:
                 output_file.close()
         # wait for everybody
