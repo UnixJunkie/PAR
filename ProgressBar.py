e5cb1d4946c359332ac7a4a8631c582828a70802
Francois Berenger <francois.berenger.fun@gmail.com>
2009-12-21 15:12:21 +0900

    removed divide by 0 bug: in the case there are no tasks to do, updating the ProgressBar has no effects

--- ProgressBar.py
+++ ProgressBar.py
@@ -19,31 +19,27 @@ class ProgressBar:
         self.updateAmount(0)  # Build progress bar string
 
     def updateAmount(self, newAmount = 0):
-        if newAmount < self.min: newAmount = self.min
-        if newAmount > self.max: newAmount = self.max
-        self.amount = newAmount
-
-        # Figure out the new percent done, round to an integer
-        diffFromMin = float(self.amount - self.min)
-        percentDone = (diffFromMin / float(self.span)) * 100.0
-        percentDone = round(percentDone)
-        percentDone = int(percentDone)
-
-        # Figure out how many hash bars the percentage should be
-        allFull = self.width - 2
-        numHashes = (percentDone / 100.0) * allFull
-        numHashes = int(round(numHashes))
-
-        # build a progress bar with hashes and spaces
-        self.progBar = "[" + '#'*numHashes + ' '*(allFull-numHashes) + "]"
-
-        # figure out where to put the percentage, roughly centered
-        percentPlace = (len(self.progBar) / 2) - len(str(percentDone))
-        percentString = str(percentDone) + "%"
-
-        # slice the percentage into the bar
-        self.progBar = (self.progBar[0:percentPlace] + percentString +
-                        self.progBar[percentPlace+len(percentString):])
+        if self.span > 0:
+            if newAmount < self.min: newAmount = self.min
+            if newAmount > self.max: newAmount = self.max
+            self.amount = newAmount
+            # Figure out the new percent done, round to an integer
+            diffFromMin = float(self.amount - self.min)
+            percentDone = (diffFromMin / float(self.span)) * 100.0
+            percentDone = round(percentDone)
+            percentDone = int(percentDone)
+            # Figure out how many hash bars the percentage should be
+            allFull = self.width - 2
+            numHashes = (percentDone / 100.0) * allFull
+            numHashes = int(round(numHashes))
+            # build a progress bar with hashes and spaces
+            self.progBar = "[" + '#'*numHashes + ' '*(allFull-numHashes) + "]"
+            # figure out where to put the percentage, roughly centered
+            percentPlace = (len(self.progBar) / 2) - len(str(percentDone))
+            percentString = str(percentDone) + "%"
+            # slice the percentage into the bar
+            self.progBar = (self.progBar[0:percentPlace] + percentString +
+                            self.progBar[percentPlace+len(percentString):])
 
     # draw progress bar only if it has changed
     def draw(self):
