b4c65e0d58b90cb4e84375048d0ec1b9eab3f694
Francois Berenger <francois.berenger.fun@gmail.com>
2010-01-22 20:07:02 +0900

    * uses logging * added DataManager class

--- DataManager.py
+++ DataManager.py
@@ -22,17 +22,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ---
 """
 
-import logging, sys
+import logging, socket, sys
+import Pyro.core, Pyro.naming
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(message)s')
-# log levels:
-#  debug
-#  info
-#  warning
-#  error
-#  critical
-#  exception
+from Chunk           import Chunk
+from MetaDataManager import MetaDataManager
+from Pyro.errors     import NamingError
+
+class DataManager:
+
+    def __init__(self, storage_file, remote_server, remote_port):
+        # when compressing, we must compress before cuting into chunks so we
+        # will have fewer chunks to transfer instead of having smaller ones
+        # (better for network latency I think)
+        self.CHUNK_SIZE = 1024*1024
+        self.data_store = storage_file
+        # FBR: create and write to it right now so we are sure it will work
+        #      later on
+        self.hostname   = socket.getfqdn()
+        self.chunks     = {}
+        logging.info('Locating Name Server...')
+        locator = Pyro.naming.NameServerLocator()
+        ns = locator.getNS(host = remote_server,
+                           port = remote_port)
+        logging.info('Located')
+        try:
+            logging.info('Locating meta_data_manager...')
+            URI = ns.resolve('meta_data_manager')
+            logging.info('Located')
+        except NamingError,x:
+            logging.fatal("Couldn't find object, nameserver says: " + x)
+            raise SystemExit
+        self.mdm = Pyro.core.getProxyForURI(URI)
+
+    def put(self, file):
+        # FBR: finish this
+        pass
+
+    def get(self):
+        pass
 
 # What are the external commands users will call on a DataManager?
 ##################################################################
@@ -55,20 +83,14 @@ def usage():
     """
     sys.exit(0)
 
-# code to use here soon
-# if os.path.isfile (self.name):
-#     try:
-#         self.size = os.path.getsize(self.name)
-#     except os.error:
-#         logging.exception ("can't get size of " + self.name)
-# else:
-#     logging.error ("no file " + self.name)
-
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(message)s')
     # FBR: - put this in an infinite loop
     #      - fork the DataManager thread out
-    #      - instantiate the MetaDataManager in parallel.py
-    #      - converse with the MetaDataManager
+    #      - instantiate the MetaDataManager in parallel.py later
+    #        and call its methods remotely
+    mdm = MetaDataManager()
     commands      = ["ls", "put", "get", "quit", "q", "exit"]
     correct_argcs = [2,3,4]
     argc = len(sys.argv)
@@ -83,21 +105,29 @@ if __name__ == '__main__':
         param_2 = sys.argv[3]
     if command == "ls":
         if argc != 2:
-            print "ls takes no argument"
+            logging.error("ls takes no argument")
             usage()
         logging.debug("going to exec: " + command)
     elif command == "put":
         if argc not in [3,4]:
-            print "put takes one or two arguments"
+            logging.error("put takes one or two arguments")
             usage()
         logging.debug("going to exec: " + command)
+        # code to use here soon
+        # if os.path.isfile (self.name):
+        #     try:
+        #         self.size = os.path.getsize(self.name)
+        #     except os.error:
+        #         logging.exception ("can't get size of " + self.name)
+        # else:
+        #     logging.error ("no file " + self.name)
     elif command == "get":
         if argc not in [3,4]:
-            print "get takes one or two arguments"
+            logging.error("get takes one or two arguments")
             usage()
         logging.debug("going to exec: " + command)
     elif command in ["quit", "q", "exit"]:
         sys.exit(0)
     else:
-        print "unknown command: " + command
+        logging.error("unknown command: " + command)
         usage()
