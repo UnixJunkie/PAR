bfb651299252efa7f2a7efc7b4367335ff0c14ed
Francois Berenger <francois.berenger.fun@gmail.com>
2010-01-22 19:14:43 +0900

    added, was in MetaDataManager initially

---
+++ MetaData.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2009, Zhang Initiative Research Unit,
+Advance Science Institute, Riken
+2-1 Hirosawa, Wako, Saitama 351-0198, Japan
+If you use and like our software, please send us a postcard! ^^
+---
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+---
+"""
+
+# How is described a file?
+##########################
+# metadata: <-- managed by MetaDataManager (only one per parallel.py server)
+# =========
+# - a name
+# - a size (of the uncompressed file)
+# - a creation time and date
+#   [optionaly]
+#   - a user
+#   - a group
+#   - permissions for UGO <-- NOTE: ACLs are fare better than UGO
+# data: <-- managed by DataManager (one per parallel.py client,
+#                                   ??? plus one for the server ???)
+#                                   the server could be run on a cluster
+#                                   frontend, some clusters don't like
+#                                   that nodes use the frontend's
+#                                   disk/network/CPU too heavily
+# maybe we should have DataManagers only inside the cluster, and maybe
+# we need a special mode (--mirror), which means this special DataManager
+# must have all data listed by the MetaDataManager
+# =====
+# - a dictionary of chunks
+class MetaData:
+    # DataManager must not create this object, he must only give all info
+    # to create it to the MetaDataManager which will create and then handle
+    # this object
+    def __init__(self, size, filename, dfs_path = None):
+        self.size          = size
+        self.name          = filename
+        self.dfs_path      = filename
+        self.creation_time = time.time()
+        self.chunks = {}
+        # FBR: chunks list must be initialized !!!
+        #      When and where?
+        if dfs_path != None:
+            self.dfs_path = dfs_path
+
+    def get_uniq_ID(self):
+        return self.dfs_path
