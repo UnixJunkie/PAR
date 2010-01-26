e83f00c57fad8d596fac46b41c62f9ae8b5d83f2
Francois Berenger <francois.berenger.fun@gmail.com>
2010-01-26 17:00:13 +0900

    start to work at least with a local data store

--- DataManager.py
+++ DataManager.py
@@ -22,41 +22,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ---
 """
 
-import commands, logging, random, socket, sys
+import commands, logging, os, random, socket, sys, thread
 # import Pyro.core, Pyro.naming
 
 from tarfile         import TarFile
+from tempfile        import TemporaryFile
 from Chunk           import Chunk
 from MetaDataManager import MetaDataManager
 # from Pyro.errors     import NamingError
 
 class DataManager:
 
-    def __init__(self, storage_file, remote_server, remote_port):
+    def __init__(self, remote_server, remote_port):
         # when compressing, we must compress before cuting into chunks so we
         # will have fewer chunks to transfer instead of having smaller ones
         # (better for network latency I think)
+        ##################################
+        # SYNCHRONIZE ACCES TO DATASTORE #
+        ##################################
+        self.lock           = thread.allocate_lock()
         self.CHUNK_SIZE     = 1024*1024
         self.data_store     = None
         self.hostname       = socket.getfqdn()
+        self.storage_file   = ("/tmp/dfs_" + os.getlogin() +
+                               "_at_" + self.hostname)
         self.chunks         = {}
-        self.temp_file      = None
-        self.temp_file_name = commands.getoutput("echo /tmp/$USER.hostname")
         try:
-            # FBR: we should use an indexed file rather than tar to avoid
-            #      using a temporary file
-            #      The index should contain begin and end offset of each chunk
-            self.data_store = TarFile(storage_file, 'w')
-            try:
-                self.temp_file = open(self.temp_file_name, 'w')
-                self.temp_file.write("DFS_STORAGE_v00\n")
-                self.temp_file.flush()
-                self.add_local_chunk(0, self.temp_file_name)
-            except:
-                logging.exception("can't create or write to: " +
-                                  self.temp_file_name)
+            # The TarFile interface forces us to use a temporary file.
+            # Maybe it makes some unnecessary data copy, however having
+            # our chunks kept in the standard tar format is too cool
+            # to be changed
+            self.lock.acquire()
+            self.data_store = TarFile(self.storage_file, 'w')
+            self.lock.release()
+            temp_file = TemporaryFile()
+            temp_file.write("DFS_STORAGE_v00\n")
+            temp_file.flush()
+            temp_file.seek(0)
+            self.add_local_chunk(0, "STORAGE_INITIALIZED", temp_file)
+            temp_file.close()
         except:
-            logging.exception("can't create or write to: " + storage_file)
+            logging.exception("can't create or write to: " + self.storage_file)
 #         logging.info('Locating Name Server...')
 #         locator = Pyro.naming.NameServerLocator()
 #         ns = locator.getNS(host = remote_server,
@@ -72,36 +78,43 @@ class DataManager:
 #        self.mdm = Pyro.core.getProxyForURI(URI)
         self.mdm = MetaDataManager()
 
-    def create_chunk_name(self, chunk_number, dfs_path):
+    def get_chunk_name(self, chunk_number, dfs_path):
         return str(chunk_number) + "/" + dfs_path
 
     def chunk_name_to_index(self, chunk_name):
         return int((chunk_name.split("/"))[0])
 
-    def add_local_chunk(self, chunk_number, dfs_path):
-        self.data_store.add(self.temp_file.name,
-                            self.create_chunk_name(chunk_number, dfs_path))
+    def add_local_chunk(self, chunk_number, dfs_path, tmp_file):
+        self.lock.acquire()
+        tar_info = (self.data_store.gettarinfo
+                    (arcname = self.get_chunk_name(chunk_number, dfs_path),
+                     fileobj = tmp_file))
+        self.data_store.addfile(tar_info, tmp_file)
+        self.data_store.fileobj.flush()
+        self.lock.release()
 
     # publish a local file into the DFS
     def put(self, filename, dfs_path = None):
         if dfs_path == None:
             dfs_path = filename
-        # FBR: add compression of added file here
-        input_file = open(filename, 'r')
+        # compression of added file hook should be here
+        input_file = open(filename, 'rb')
         try:
             file_size = 0
-            nb_chunks = 0
+            chunk_index = 0
             read_buff = input_file.read(self.CHUNK_SIZE)
             while read_buff != '':
                 file_size += len(read_buff)
-                self.temp_file.truncate(0)
-                self.temp_file.write(read_buff)
-                self.temp_file.flush()
-                self.add_local_chunk(nb_chunks, dfs_path)
-                nb_chunks += 1
+                temp_file = TemporaryFile()
+                temp_file.write(read_buff)
+                temp_file.flush()
+                temp_file.seek(0)
+                self.add_local_chunk(chunk_index, dfs_path, temp_file)
+                temp_file.close()
+                chunk_index += 1
                 read_buff = input_file.read(self.CHUNK_SIZE)
             self.mdm.publish_meta_data(dfs_path, self.hostname,
-                                       file_size, nb_chunks)
+                                       file_size, chunk_index)
         except:
             logging.exception("problem while reading " + filename)
         input_file.close()
@@ -115,7 +128,7 @@ class DataManager:
         non_local_chunks = []
         # shuffle should increase pipelining and parallelization
         # of chunk transfers
-        for k in all_chunks.keys:
+        for k in all_chunks.keys():
             source_hosts = all_chunks[k]
             # FBR: linear search instead of instantaneous lookup...
             if self.hostname not in source_hosts:
@@ -124,18 +137,28 @@ class DataManager:
         # FBR: TODO download them then publish current host as hosting
         #      this chunks too
         # dump all chunk from local store in the right order
-        output_file = open(fs_output_path, 'w')
+        original_dfs_path = dfs_path
+        output_file = open(fs_output_path, 'wb')
         try:
+            self.lock.acquire()
+            read_only_data_store = TarFile(self.storage_file, 'r')
+            self.lock.release()
+            if dfs_path.startswith('/'):
+                dfs_path = dfs_path[1:]
             for i in range(meta_info.nb_chunks):
-                c = self.create_chunk_name(i, dfs_path)
-                untared_file = self.data_store.extractfile(c)
+                c = self.get_chunk_name(i, dfs_path)
+                self.lock.acquire()
+                untared_file = read_only_data_store.extractfile(c)
+                self.lock.release()
                 if untared_file == None:
                     logging.fatal("could not extract " + c +
                                   " from local store")
                 else:
-                    output_file.write(untared_file.read)
+                    output_file.write(untared_file.read())
         except:
-            logging.exception("problem while writing to " + fs_output_path)
+            logging.exception("problem while writing " + original_dfs_path +
+                              " to " + fs_output_path)
+        read_only_data_store.close()
         output_file.close()
 
 # What are the external commands users will call on a DataManager?
@@ -165,15 +188,15 @@ if __name__ == '__main__':
     # FBR: - put this in an infinite loop
     #      - fork the DataManager thread out
     #      - find a way to communicate with him locally after he was forked
-    print commands.getoutput("echo 0 && date")
-    dm = DataManager("/tmp/storage.tar", commands.getoutput("hostname"), 9090)
-    print commands.getoutput("echo 1 && date")
+    print commands.getoutput("echo 0 `date`")
+    dm = DataManager(commands.getoutput("hostname"), 9090)
+    print commands.getoutput("echo 1 `date`")
     dm.put("/tmp/big_file")
-    print commands.getoutput("echo 2 && date")
+    print commands.getoutput("echo 2 `date`")
     print(dm.mdm.ls())
-#     print commands.getoutput("echo 3 && date")
-#     dm.get("/tmp/big_file", "/tmp/from_dfs")
-#     print commands.getoutput("echo 4 && date")
+    print commands.getoutput("echo 3 `date`")
+    dm.get("/tmp/big_file", "/tmp/big_file_from_dfs")
+    print commands.getoutput("echo 4 `date`")
 #     commands      = ["ls", "put", "get", "quit", "q", "exit"]
 #     correct_argcs = [2,3,4]
 #     argc = len(sys.argv)
