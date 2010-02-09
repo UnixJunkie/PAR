6e085a85c563a6a3ccd4e2583880b136a84ad94a
Francois Berenger <francois.berenger.fun@gmail.com>
2010-02-09 20:11:46 +0900

    added some other tests too

--- src/profile_DM.py
+++ src/profile_DM.py
@@ -5,14 +5,25 @@ import profile
 from DataManager import DataManager
 
 def uput_prof(dm):
-    dm.put("/tmp/db.tar", verify = False)
+    dm.put("/tmp/par/512", verify = False)
 
 def get_prof(dm):
-    dm.get("/tmp/db.tar", "/tmp/out")
+    dm.get("/tmp/par/512", "/tmp/out")
+
+# FBR: test 1
+# for i in range(10):
+#     dm = DataManager(profiling = True)
+#     uput_prof(dm)
 
 # dm = DataManager(profiling = True)
 # profile.run('uput_prof(dm)')
 
+# FBR: test 2
+# dm = DataManager(profiling = True)
+# dm.put("/tmp/par/512", verify = False)
+# for i in range(10):
+#     get_prof(dm)
+
 dm = DataManager(profiling = True)
-dm.put("/tmp/db.tar", verify = False)
+dm.put("/tmp/par/512", verify = False)
 profile.run('get_prof(dm)')
