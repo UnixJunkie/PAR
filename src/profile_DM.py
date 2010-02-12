dc22ac207b82f0d27f04e6844cad8d27dde07c53
Francois Berenger <francois.berenger.fun@gmail.com>
2010-02-12 11:24:10 +0900

    no more profiling param

--- src/profile_DM.py
+++ src/profile_DM.py
@@ -12,18 +12,18 @@ def get_prof(dm):
 
 # FBR: test 1
 # for i in range(10):
-#     dm = DataManager(profiling = True)
+#     dm = DataManager()
 #     uput_prof(dm)
 
-# dm = DataManager(profiling = True)
+# dm = DataManager()
 # profile.run('uput_prof(dm)')
 
 # FBR: test 2
-# dm = DataManager(profiling = True)
+# dm = DataManager()
 # dm.put("/tmp/par/512", verify = False)
 # for i in range(10):
 #     get_prof(dm)
 
-dm = DataManager(profiling = True)
+dm = DataManager()
 dm.put("/tmp/par/512", verify = False)
 profile.run('get_prof(dm)')
