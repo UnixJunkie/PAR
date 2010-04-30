94ce7e14349ef873e122661601b25b9c097ca2dc
Francois Berenger <francois.berenger.fun@gmail.com>
2010-04-30 13:34:32 +0900

    must use export instead of just setting the variable

--- bin/par.sh
+++ bin/par.sh
@@ -2,8 +2,8 @@
 
 #set -x # debug
 
-PAR_ROOT=~/src/par       # where you downloaded par
-PYTHONPATH=$PAR_ROOT/lib # where par's Pyro is
-PATH=/usr/local/src/Python-2.4.6/bin:$PATH # to find python2.4 if it is
-                                           # not the default one
-$PAR_ROOT/src/parallel.py $*
+PAR_ROOT=~/src/par              # where you downloaded par
+export PYTHONPATH=$PAR_ROOT/lib # where par's Pyro is
+export PATH=/usr/local/src/Python-2.4.6/bin:$PATH # to find python2.4 if it is
+                                                  # not the default one
+$PAR_ROOT/src/parallel.py $*    # call it
