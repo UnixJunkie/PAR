c603b3a3d7ce1ab92e44d5651ad60ab157dc6887
Francois Berenger <berenger@riken.jp>
2012-04-27 10:18:14 +0900

    removed change of PATH to find python2.4

--- bin/par.sh
+++ bin/par.sh
@@ -4,6 +4,4 @@
 
 PAR_ROOT=~/src/par              # where you downloaded par
 export PYTHONPATH=$PAR_ROOT/lib # where par's Pyro is
-export PATH=/usr/local/src/Python-2.4.6/bin:$PATH # to find python2.4 if it is
-                                                  # not the default one
 $PAR_ROOT/src/parallel.py "$@"  # call it
